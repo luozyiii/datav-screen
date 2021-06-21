4df45996547776fe4d2501d917e23b0bd2688b98
罗智毅 <zhiyiluo@drgou.com>
2021-06-21 18:03:23 +0800

    svg loading组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,1 @@
-export { default as Demo } from './Demo';
+export { default as Loading } from './Loading';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
+import { Loading } from '@/components';
+
 import './index.less';
 
 export default function IndexPage() {
-  return <div className="app">home</div>;
+  return (
+    <div className="app">
+      <Loading />
+      <Loading
+        width="100"
+        height="100"
+        outsideColor="red"
+        insideColor="blue"
+        duration="1"
+      />
+    </div>
+  );
 }
